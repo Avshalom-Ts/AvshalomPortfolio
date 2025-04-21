473902be17dcc4e20f2f0eeccb3381a8cfb074e8
Avshalom-Z <avshalomz.ts@gmail.com>
2025-04-21 19:39:43 +0300

    Update Vite preview configuration to set host to '0.0.0.0'

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   preview:{
+    host: '0.0.0.0',
     allowedHosts: [
       'avshalom-folio.com'
     ]
