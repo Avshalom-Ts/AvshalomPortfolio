51b32e4eb1ef9760728d74341b3f68dcbcb3558e
Avshalom-Z <avshalomz.ts@gmail.com>
2025-04-21 18:43:06 +0300

    Update Vite preview configuration to use localhost and specify allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   preview:{
-    host: 'avshalom-folio.com',
-    open: true,
+    host: 'localhost',
+    allowedHosts: [
+      'avshalom-folio.com',
+      'www.avshalom-folio.com',
+      'localhost',
+    ]
   },
   plugins: [
     react(),
